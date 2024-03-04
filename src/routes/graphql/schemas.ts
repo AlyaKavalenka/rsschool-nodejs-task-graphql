128f30178317060835315d5f11073cd185363e3c
Alya Kovalenko <kovalenkoalinam@gmail.com>
2024-03-04 21:47:34 +0300

    feat: start queris

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,8 @@
 import { Type } from '@fastify/type-provider-typebox';
+import { PrismaClient } from '@prisma/client';
+import { GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+
+const prisma = new PrismaClient();
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +22,120 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+const MemberType = new GraphQLObjectType({
+  name: 'memberType',
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+    },
+    discount: {
+      type: GraphQLString,
+    },
+    postsLimitPerMonth: {
+      type: GraphQLString,
+    },
+  }),
+});
+
+const PostType = new GraphQLObjectType({
+  name: 'posts',
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+    },
+    title: {
+      type: GraphQLString,
+    },
+    content: {
+      type: GraphQLString,
+    },
+  }),
+});
+
+const UsersType = new GraphQLObjectType({
+  name: 'users',
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+    },
+    name: {
+      type: GraphQLString,
+    },
+    balance: {
+      type: GraphQLString,
+    },
+  }),
+});
+const ProfilesType = new GraphQLObjectType({
+  name: 'profiles',
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+    },
+    isMale: {
+      type: GraphQLString,
+    },
+    yearOfBirth: {
+      type: GraphQLString,
+    },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    memberTypes: {
+      type: new GraphQLList(MemberType),
+      args: {},
+      async resolve() {
+        const res = await prisma.memberType.findMany(); // return all members
+        return res;
+      },
+    },
+    memberType: {
+      type: MemberType,
+      args: { id: { type: GraphQLString } },
+      async resolve(parent, args: { id: string }) {
+        if (args.id) {
+          const res = await prisma.memberType.findUnique({
+            // return member by id
+            where: {
+              id: args.id,
+            },
+          });
+          return res;
+        }
+        return {};
+      },
+    },
+    posts: {
+      type: new GraphQLList(PostType),
+      args: {},
+      async resolve() {
+        const res = await prisma.post.findMany().then((res) => res);
+        return res;
+      },
+    },
+    users: {
+      type: new GraphQLList(UsersType),
+      args: {},
+      async resolve() {
+        const res = await prisma.user.findMany();
+        return res;
+      },
+    },
+    profiles: {
+      type: new GraphQLList(ProfilesType),
+      args: {},
+      async resolve() {
+        const res = await prisma.profile.findMany();
+        return res;
+      },
+    },
+  },
+});
+
+export const schema = new GraphQLSchema({
+  query: RootQuery,
+});
